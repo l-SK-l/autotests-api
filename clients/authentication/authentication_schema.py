e499c5eefd55a813059b6a68d6ca03aa87ca920a
Sergei Kozlov <sergeo.kozlov@gmail.com>
2025-05-02 10:30:57 +0200

    Написание API автотестов используя Pytest. Практика написания API автотестов

--- clients/authentication/authentication_schema.py
+++ clients/authentication/authentication_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 from tools.fakers import fake
 
 
@@ -6,6 +6,8 @@ class TokenSchema(BaseModel):
     """
     Описание структуры аутентификационных токенов.
     """
+    model_config = ConfigDict(populate_by_name=True)
+
     token_type: str = Field(alias="tokenType")
     access_token: str = Field(alias="accessToken")
     refresh_token: str = Field(alias="refreshToken")
@@ -31,4 +33,6 @@ class RefreshRequestSchema(BaseModel):
     """
     Описание структуры запроса для обновления токена.
     """
+    model_config = ConfigDict(populate_by_name=True)
+
     refresh_token: str = Field(alias="refreshToken", default_factory=fake.sentence)
