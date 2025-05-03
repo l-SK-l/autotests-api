928ecadaeb0aceec64722287b7758d029f0fb905
Sergei Kozlov <sergeo.kozlov@gmail.com>
2025-05-03 15:15:54 +0200

    Параметризация Pytest. Практика работы с pytest параметризацией

--- tests/test_users.py
+++ tests/test_users.py
@@ -5,13 +5,26 @@ import pytest
 from clients.authentication.authentication_client import AuthenticationClient
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
 from clients.users.private_users_client import PrivateUsersClient
-from clients.users.public_users_client import PublicUsersClient
-from clients.users.users_schema import CreateUserRequestSchema, GetUserResponseSchema
+from clients.users.public_users_client import PublicUsersClient, get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from tools.fakers import fake
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response as assert_get_user_response
 from fixtures.users import UserFixture
+
+
+@pytest.mark.parametrize("email", ["mail.ru", "gmail.com", "example.com"])
+def test_create_user(email: str, public_users_client: PublicUsersClient):
+    request = CreateUserRequestSchema(email=fake.email(domain=email))
+    response = public_users_client.create_user_api(request)
+    response_data = CreateUserResponseSchema.model_validate_json(response.text)
+
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_get_user_response(request, response_data)
+
+    validate_json_schema(response.json(), response_data.model_json_schema())
 
 
 @pytest.mark.regression
