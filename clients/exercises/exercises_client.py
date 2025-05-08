c64f993acd0406625161f574efba32572381f11f
Sergei Kozlov <sergeo.kozlov@gmail.com>
2025-05-08 10:27:00 +0200

    Улучшение работы с API клиентами с помощью Enum для URI. Практика работы с использованием APIRoutes в API-клиентах

--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -4,6 +4,7 @@ from httpx import Response
 from clients.api_client import APIClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, GetExerciseResponseSchema, GetExercisesQuerySchema, GetExercisesResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
 from clients.private_http_builder import AuthenticationUserSchema, get_private_http_client
+from tools.routes import APIRoutes
 
 
 class ExercisesClient(APIClient):
@@ -19,7 +20,7 @@ class ExercisesClient(APIClient):
         :param query: Словарь с courseId.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get("/api/v1/exercises", params=query.model_dump(by_alias=True))
+        return self.get(f"{APIRoutes.EXERCISES}", params=query.model_dump(by_alias=True))
 
     def get_exercises(self, query: GetExercisesQuerySchema) -> GetExercisesResponseSchema:
         response = self.get_exercises_api(query)
@@ -33,7 +34,7 @@ class ExercisesClient(APIClient):
         :param exercise_id: Идентификатор упражнения.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get(f"/api/v1/exercises/{exercise_id}")
+        return self.get(f"{APIRoutes.EXERCISES}/{exercise_id}")
 
     def get_exercise(self, exercise_id: str) -> GetExerciseResponseSchema:
         response = self.get_exercise_api(exercise_id)
@@ -48,7 +49,7 @@ class ExercisesClient(APIClient):
         description, estimatedTime.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.post("/api/v1/exercises", json=request.model_dump(by_alias=True))
+        return self.post(f"{APIRoutes.EXERCISES}", json=request.model_dump(by_alias=True))
 
     def create_exercise(self, request: CreateExerciseRequestSchema) -> CreateExerciseResponseSchema:
         response = self.create_exercise_api(request)
@@ -65,7 +66,7 @@ class ExercisesClient(APIClient):
         description, estimatedTime.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.patch(f"/api/v1/exercises/{exercise_id}", json=request.model_dump(by_alias=True))
+        return self.patch(f"{APIRoutes.EXERCISES}/{exercise_id}", json=request.model_dump(by_alias=True))
 
     def update_exercise(self, exercise_id: str,
                          request: UpdateExerciseRequestSchema) -> UpdateExerciseResponseSchema:
@@ -80,7 +81,7 @@ class ExercisesClient(APIClient):
         :param exercise_id: Идентификатор упражнения.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.delete(f"/api/v1/exercises/{exercise_id}")
+        return self.delete(f"{APIRoutes.EXERCISES}/{exercise_id}")
 
     def delete_exercise(self, exercise_id: str) -> None:
         self.delete_exercise_api(exercise_id)
